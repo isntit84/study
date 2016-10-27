d91a42041d47baa5e5ace4de728b4c8701577ca4
isntit84 <isntit84@naver.com>
2016-10-27 15:52:14 +0900

    Member, Product project를 작성 하였습니다.

---
+++ workspace/Member/src/Service/MemberService.java
@@ -0,0 +1,64 @@
+package Service;
+
+import java.util.ArrayList;
+
+import VO.MemberVO;
+
+public class MemberService {
+
+	private ArrayList<MemberVO> list = new ArrayList<MemberVO>();
+
+	public void addMemberVO(MemberVO memberVO) {
+		boolean flag = false;
+		for (int i = 0; i < list.size(); i++) {
+			MemberVO Mem = (MemberVO) list.get(i);
+			if (memberVO.equals(Mem.getId()))
+				flag = true;
+			break;
+
+		}
+		if (flag == false)
+			list.add(memberVO);
+		else
+			System.out.println("이미 등록된 아이디 입니다.");
+	}
+
+	public void printAll() {
+		System.out.println(list);
+	}
+
+	public MemberVO findMemberVO(String id) {
+		MemberVO pro = null;
+		for (int i = 0; i < list.size(); i++) {
+			MemberVO pro1 = (MemberVO) list.get(i);
+			if (id.equals(pro1.getId())) {
+				pro = pro1;
+			}
+		}
+		return pro;
+
+	}
+
+	public void deleteMemberVo(String id) {
+		for (int i = 0; i < list.size(); i++) {
+			MemberVO pro1 = (MemberVO) list.get(i);
+			if (id.equals(pro1.getId())) {
+				list.remove(i);
+			}
+		}
+	}
+
+	public boolean updateMemberVO(MemberVO memberVO) {
+		boolean flag = false;
+		for (int i = 0; i < list.size(); i++) {
+			MemberVO Mem = (MemberVO) list.get(i);
+			if (Mem.getId().equals(memberVO.getId())) {
+				list.set(i, memberVO);
+				flag = true;
+				break;
+			}
+		}
+		return flag;
+	}
+
+}
---
+++ workspace/Member/src/Test/TestMemberService.java
@@ -0,0 +1,36 @@
+package Test;
+
+import Service.MemberService;
+import VO.MemberVO;
+
+public class TestMemberService {
+
+	public static void main(String[] args) {
+
+		MemberService service = new MemberService();
+
+		service.addMemberVO(new MemberVO("java", "손연재", "111", "구로"));
+		service.addMemberVO(new MemberVO("oracle", "김연아", "222", "군포"));
+		service.addMemberVO(new MemberVO("JSP", "홍명보", "333", "파주"));
+		service.addMemberVO(new MemberVO("Ajax", "이청용", "444", "영국"));
+		service.addMemberVO(new MemberVO("Spring", "박지성", "555", "수원"));
+		service.printAll();
+
+		MemberVO vo = service.findMemberVO("java");
+
+		if (vo != null)
+			System.out.println(vo);
+		else
+			System.out.println("회원이 아닙니다.");
+
+		service.deleteMemberVo("Ajax");
+		service.printAll();
+
+		boolean result = service.updateMemberVO(new MemberVO("Jquery", "류현진", "666", "인천"));
+		if (result)
+			System.out.println("수정완료");
+		else
+			System.out.println("상품이 없어서 수정불가");
+		service.printAll();
+	}
+}
