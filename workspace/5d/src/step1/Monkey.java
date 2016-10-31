1a605537c5866856b9ce1adc374525f4700a4ae0
isntit84 <isntit84@naver.com>
2016-10-31 14:10:38 +0900

    중복되어 5d 프로젝트를 삭제하였습니다.

--- workspace/5d/src/step1/Monkey.java
+++
@@ -1,7 +0,0 @@
-package step1;
-
-public class Monkey extends Animal{
-	public void play() {
-		System.out.println("나무타고 놀다");
-	}
-}
--- workspace/5d/src/step1/Tiger.java
+++
@@ -1,7 +0,0 @@
-package step1;
-
-public class Tiger extends Animal {
-	public void play() {
-		System.out.println("¾îÈïÇÏ°í ³î´Ù");
-	}
-}
